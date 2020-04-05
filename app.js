e28093561a77ec4a21782623152bf0174dd2c68a
Trust1752 <joao.l.santos@outlook.com.br>
2020-04-05 15:36:05 -0300

    modificacao app.js

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-/**
- *  * Arquivo: app.js
- *   * Descrição: arquivo principal e responsável pela execução da aplicação.
- *    * Data: 05/07/2019
- *     * Author: Glaucia Lemos
- *      */
-
 const express = require('express');
-
 const app = express();
-
 const port = process.env.PORT || 3000;
-
 app.get('/', (req, res) => {
 	  res.status(200).send({
 		      success: 'true',
-		      message: 'Hello World API Node.JS!',
+		      message: 'Hello World API',
 		      version: '1.0.0',
 		    });
 });
-
 app.listen(port);
 console.log('Aplicação executando na porta ', port);
